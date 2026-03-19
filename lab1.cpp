6cf6c54ea42374a7df9ed113b5d4d879e6dae47e
smchugunova-arch <s.m.chugunova@icloud.com>
2026-03-19 22:38:08 +0700

    lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cout << "Введите стороны треугольника: ";
+    cin >> a >> b >> c;
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        cout << "Треугольник не существует" << endl;
+        return 1;
+    }
+
+    double P = a + b + c;
+
+    double p = P / 2;
+
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << "Периметр: " << P << endl;
+    cout << "Площадь: " << S << endl;
+
+    if (a == b || a == c || b == c) {
+        cout << "Треугольник равнобедренный" << endl;
+    } else {
+        cout << "Треугольник не равнобедренный" << endl;
+    }
+
+    return 0;
+}
