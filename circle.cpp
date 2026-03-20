809a4dd5cf05e90acc50ea97b6313d9946cf188e
Lilit-soul <rubyornot2000@gmail.com>
2026-03-20 15:37:20 +0700

    second

--- circle.cpp
+++ circle.cpp
@@ -3,26 +3,49 @@
 #include <cmath>
 using namespace std;
 
-int main(){
+void triangle();
 
+int main(){
+    // Первая попытка
     const double PI = acos(-1.0); // число пи (аркосинус -1)
     double radius;
     double corner;
     int rounding;
 
+    cout << "----Окружность----" << "\n";
+
     cout << "Введите радиус: ";
     cin >> radius;
+    if (radius < 0) {
+            cout << "Радиус не может быть отрицательным!" << endl;
+            return 0;
+        }
     cout << "Введите угол: ";
     cin >> corner;
-    cout << "Введите число для округления: ";
+    if (corner < 0) {
+        cout << "Угол не может быть отрицательным!" << endl;
+        return 0;
+    }
+    if (corner >360) {
+            cout << "Угол не может быть больше 360!" << endl;
+            return 0;
+        }
+    cout << "Введите положительное число для округления: ";
     cin >> rounding;
+    if (rounding < 0) {
+        cout << "Округление не может быть отрицательным!" << endl;
+        return 0;
+    }
 
     double circuit = 2 * PI * radius;// Длина окружности
     double area = PI * radius * radius;// Площадь круга
     double sector = (corner / 360) * area;// Площадь кругового сектора
 
+    cout << "---------------" << "\n";
     cout << "Длина окружности: " << setprecision(rounding) << circuit << "\n";
-    cout << "Площадь круга: "<< setprecision(rounding)<< area << "\n";
-    cout << "Площадь кругового сектора: " <<setprecision(rounding)<< sector << "\n";
-    
+    cout << "Площадь круга: " << setprecision(rounding) << area << "\n";
+    cout << "Площадь кругового сектора: " << setprecision(rounding) << sector << "\n";
+    triangle();
+    return 0;    
 }
+
